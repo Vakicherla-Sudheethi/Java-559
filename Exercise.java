cc445cbf73aa84b00148ace95b35ec897a155bd2
Vakicherla.Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-02-06 11:41:19 +0530

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+class Exercise
+{
+public static void main(String args[])
+{
+int i=10;
+//i=i+2;//14
+//i+=2;//14
+//i-=2;//8
+//i*=2;//20
+//System.out.println(i);
+//---BYTE----
+byte b=8;
+b*=2.5;//20
+System.out.println(b);
+//---Binary to decimal conversion---
+int j=0B101_00_00;
+int k=0b101;
+System.out.println(j);
+
+}
+}
---
+++ World.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+class World
+{
+public static void main(String args[])
+{
+//String name="fdsdfh";
+//System.out.println(name);
+//System.out.println("Hello world");
+//final int num=5;
+//num=34;
+//System.out.println(num);
+int i,j,k;
+i = 3;
+j=4;
+k=i+j;
+//---print statements---
+//System.out.println(k);
+//System.out.println("The addition is"+" "+k);
+//System.out.println("Addition is: "+i+"and "+j+"is:"+k);
+//System.out.printf("the addition of %d and %d is %d",i,j,k);
+
+}
+}
